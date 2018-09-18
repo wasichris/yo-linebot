0d2bc4e859c3db60d0218681ec066059813a8f3e
Chris Chen <wasi.chris@gmail.com>
2018-09-18 16:36:29 +0800

    setup for linebot

--- app.js
+++ app.js
@@ -10,6 +10,17 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+// setup for linebot
+var bot = linebot({
+  channelId: '1608464162',
+  channelSecret: '0892ebf2b065e59e00545dacf616d0ac',
+  channelAccessToken: 'eI59B9rz4Hov7e2AcfzIxRPprE+JeBWR95eGN9KzEhDOfo9I3KOM+7DErclefGn866g9/JfDVwAEInR/OW2LEchWtqMIyQgo7/RKmK5zh6Z4N1hYUR7WXFNJzlAKdJYZ3/+Epnz3W0JIdiZA65zGEAdB04t89/1O/w1cDnyilFU='
+});
+
+bot.on('message', function(event) {
+  console.log(event); //把收到訊息的 event 印出來看看
+});
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
