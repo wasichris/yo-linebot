9605c3335641464034a9d08b3ae0c16da2ef0bf8
Chris Chen <wasi.chris@gmail.com>
2018-10-01 16:29:16 +0800

    update channelAccessToken

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ var linebot = require('linebot');
 var bot = linebot({
   channelId: '1608464162',
   channelSecret: '0892ebf2b065e59e00545dacf616d0ac',
-  channelAccessToken: 'eI59B9rz4Hov7e2AcfzIxRPprE+JeBWR95eGN9KzEhDOfo9I3KOM+7DErclefGn866g9/JfDVwAEInR/OW2LEchWtqMIyQgo7/RKmK5zh6Z4N1hYUR7WXFNJzlAKdJYZ3/+Epnz3W0JIdiZA65zGEAdB04t89/1O/w1cDnyilFU='
+  channelAccessToken: 'sLrxLV6NcQVnOMRl5/a2E9DotMaLmwW5+t243LOqn6QJc3Ve4bfzr1TjqONSFSw/66g9/JfDVwAEInR/OW2LEchWtqMIyQgo7/RKmK5zh6aEcqqkg6ru8daPoqiGfATqQzP4sNeKD4uFutftzTTzowdB04t89/1O/w1cDnyilFU='
 });
 
 bot.on('message', function(event) {
